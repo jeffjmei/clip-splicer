ca686bc0c1868259ec34444d3a7434a575ca27c6
jeffjmei <jeffreymei926@gmail.com>
2025-05-07 22:25:42 -0700

    working whisper implementation

--- clip-splicer.py
+++ clip-splicer.py
@@ -6,14 +6,30 @@
 #import whisper # for processing video
 #from moviepy.editor import VideoFileClip, concatenate_videoclips # video editing
 import subprocess # for importing videos
+import whisper # for video transcription
 
 # ===============
 # Download Video 
 # ===============
-url = 'https://www.youtube.com/watch?v=9b4OAlpPhNA'
-yt = YouTube(url)
 
 # Download the video to the current directory
+url = 'https://www.youtube.com/watch?v=9b4OAlpPhNA' # trump interview
+url = 'https://www.youtube.com/watch?v=pY23ctQYGl4' # columbia arrests
 subprocess.run(['yt-dlp', url])
 
+# Transcribe Data
+model = whisper.load_model("base")  # or "small", "medium", "large" for more accuracy
+result = model.transcribe("columbia-arrests.mkv")
+print(result["text"])
+
+# Print or inspect segments with timestamps
+for segment in result["segments"]:
+    print(f"[{segment['start']:.2f} - {segment['end']:.2f}] {segment['text']}")
+
+# Keyword Search
+keywords = ["Palestine", "Columbia"]
+highlight_segments = [
+    seg for seg in result["segments"]
+    if any(k in seg["text"].lower() for k in keywords)
+]
 
